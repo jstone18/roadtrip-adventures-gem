9772a0ab27e592d508ec2eb2fa0d4a562eb6c12c
jstone18 <jason.stone0808@gmail.com>
2019-01-23 22:38:10 -0500

    Initial commit

---
+++ lib/roadtrip_adventures.rb
@@ -0,0 +1,6 @@
+require "roadtrip_adventures/version"
+
+module RoadtripAdventures
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/roadtrip_adventures/version.rb
@@ -0,0 +1,3 @@
+module RoadtripAdventures
+  VERSION = "0.1.0"
+end
