f1cb63e6fa9efa9bc89dae88b9697d8759d2a1af
jstone18 <jason.stone0808@gmail.com>
2019-01-27 19:19:50 -0500

    Updated file requirements

--- lib/roadtrip_adventures.rb
+++ lib/roadtrip_adventures.rb
@@ -1,4 +1,10 @@
-require "roadtrip_adventures/version"
+require "nokogiri"
+require "open-uri"
+
+require_relative "./roadtrip_adventures/version"
+require_relative "./roadtrip_adventures/cli"
+require_relative "./roadtrip_adventures/destination"
+require_relative "./roadtrip_adventures/adventure"
 
 module RoadtripAdventures
   class Error < StandardError; end
