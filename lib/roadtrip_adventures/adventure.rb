8b377d8125be0d1b6c308281f45be921f8e87b09
jstone18 <jason.stone0808@gmail.com>
2019-01-28 17:34:01 -0500

    Updated with #scrape_adventures class method

--- lib/roadtrip_adventures/adventure.rb
+++ lib/roadtrip_adventures/adventure.rb
@@ -2,4 +2,22 @@ class RoadtripAdventures::Adventure
 
   attr_accessor :name, :url
 
+
+  # Scrape data using url to create new Adventure objects
+  # push new objects into an array
+  def self.scrape_adventures
+    adventures_list = []
+    doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/usa/new-york-city"))
+    adventures = doc.css("article.tours a")
+
+    adventures.map.with_index{ |adventure, index|
+    new_adventure = RoadtripAdventures::Adventure.new
+    new_adventure.name = adventures.css("h3").text
+    new_adventure.url = adventures[index].attr("href")
+    adventures_list = new_adventure
+    }
+    # binding.pry
+  end
+
+
 end
