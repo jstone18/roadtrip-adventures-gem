7e21010354f1f6a5744b31f6d73411f74487485a
jstone18 <jason.stone0808@gmail.com>
2019-01-27 19:32:43 -0500

    Added cli class with some basic functionality

---
+++ lib/roadtrip_adventures/cli.rb
@@ -0,0 +1,20 @@
+class RoadtripAdventures::CLI
+
+  def call
+    list_destinations
+
+  end
+
+  def list_destinations
+    puts "Todays Destinations."
+  end
+
+  def dest_menu
+  end
+
+
+  def goodbye
+    puts "So long!! May your journey be filled with new adventures and excitement"
+  end
+
+end
