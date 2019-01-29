85a64bbfb24e2a4a502bc1cbeafd9b9889e3c356
jstone18 <jason.stone0808@gmail.com>
2019-01-28 22:00:04 -0500

    Updated cli with #menu, functionality still needs work.

--- lib/roadtrip_adventures/cli.rb
+++ lib/roadtrip_adventures/cli.rb
@@ -1,18 +1,42 @@
 class RoadtripAdventures::CLI
 
   def call
+    welcome
     list_destinations
-    RoadtripAdventures::Destination.list_destination_names
-    dest_menu
-    RoadtripAdventures::Adventure.list_adventure_names
+    menu
+  end
+
+  def welcome
+    puts "Welcome to Roadtrip Adventures!"
   end
 
   def list_destinations
-    puts "Todays Destinations."
+    puts "Choose the destination you'd like to explore further."
+    RoadtripAdventures::Destination.list_destination_names
+    puts "********************************************************"  # Divider
   end
 
-  def dest_menu
+  def menu
+    input = nil
+
+    while input != "exit"
+      puts "Make a selection from 1-10"
+      input = gets.strip.to_i
+
+      if input > 0 && input < 11
+        puts "********************************************************"
+
+        puts "These are the Top 6 adventures!"
+        puts RoadtripAdventures::Adventure.list_adventure_names[input-1]
+
+        puts "Which adventure would you like more information for? 1 - 6"
+        input = gets.strip.to_i
+      else
+        puts "You've made an invalid choice! Please try again or 'exit' to leave"
+      end
+    end
   end
+
 
 
   def goodbye
