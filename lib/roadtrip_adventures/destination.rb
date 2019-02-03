d08b6c7127768401b839eb1724213f7558704ca5
jstone18 <jason.stone0808@gmail.com>
2019-02-03 11:51:35 -0500

    Refactored, added #initialize and #url_for_destination_name

--- lib/roadtrip_adventures/destination.rb
+++ lib/roadtrip_adventures/destination.rb
@@ -1,37 +1,18 @@
+class RoadtripAdventures::Destination
 
-  class RoadtripAdventures::Destination
+  attr_accessor :name, :url
+  @@destinations = []
 
-    attr_accessor :name, :url
-    @@all_destinations = []
-
-    #1. New York City   6. New Orleans
-    #2. Los Angeles     7. Washington,DC
-    #3. San Francisco   8. Boston
-    #4. Chicago         9. Miami
-    #5. Seattle        10. Las Vegas
-
-    def self.all_destinations
-      self.class.all_destinations
-    end
-
-
-    def self.scrape_destinations
-      doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/usa"))
-      destinations = doc.css("a.tlist__secondary-link")
-
-      destinations.each_with_index{ |destination, index|
-        if index < 10 then
-            new_destination = RoadtripAdventures::Destination.new
-            new_destination.name = destinations[index].text
-            new_destination.url = destinations[index].attr("href")
-            @@all_destinations << new_destination
-        end
-          }
-    end
-
-    def self.list_destination_names
-      self.scrape_destinations
-      @@all_destinations.map.with_index{|d, i| puts "#{i+1} #{d.name}" }
-    end
-
+  def initialize
+    @@destinations << self
   end
+
+  def self.all
+    @@destinations
+  end
+
+  def self.url_for_destination_name(destination_name)
+  "https://www.lonelyplanet.com/#{destination_name.downcase.gsub!(/[\s,]+/,"-")}/"
+  end
+
+end
