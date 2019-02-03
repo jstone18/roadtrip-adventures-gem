d44ee003ae22217786d7610d23600c1059949b86
jstone18 <jason.stone0808@gmail.com>
2019-02-03 11:45:52 -0500

    Refactored code

--- lib/roadtrip_adventures/cli.rb
+++ lib/roadtrip_adventures/cli.rb
@@ -2,8 +2,8 @@ class RoadtripAdventures::CLI
 
   def call
     welcome
+    RoadtripAdventures::Scraper.scrape_destinations
     list_destinations
-    dest_menu
   end
 
   def welcome
@@ -11,43 +11,40 @@ class RoadtripAdventures::CLI
   end
 
   def list_destinations
-    puts "Choose the destination you'd like to explore further."
-    puts "********************************************************"
-    RoadtripAdventures::Destination.list_destination_names
-    puts "********************************************************"  # Divider
+     puts "Please select a destination from 1-10"
+
+     destination_arr = RoadtripAdventures::Destination.all
+     puts  destination_arr.map.with_index{|destination, index| "\t#{index+1}. #{destination.name}"}
+     puts "********************************************************"
+     select_destination(destination_arr)
   end
 
-  def dest_menu
-    dest_select = nil
+  def select_destination(destination_arr)
+    input = nil
+    while input != 'exit'
+      input = gets.to_i
 
-    puts "Make a selection from 1-10:"
-    dest_select = gets.strip
-
-    while dest_select != "exit"
-
-      if dest_select.to_i > 0 && dest_select.to_i < 11
-
-        puts "These are the Top 6 adventures!"
+      if input > 0 && input < 11
         puts "********************************************************"
-        puts RoadtripAdventures::Adventure.list_adventure_names[dest_select.to_i-1]
-        puts "********************************************************"
-        adv_menu
-
+        destination_name = destination_arr[input-1].name.strip
+        puts "Here are the top 6 adventures for #{destination_name.upcase}\n"
+        list_adventures(destination_name)
       else
         puts "You've made an invalid choice! Make a selection from 1-10 or 'exit' to leave"
-        dest_select = gets.strip
+        input = gets.strip
       end
+      goodbye if input === "exit"
     end
-    goodbye
   end
 
-  def adv_menu
-    puts "Which adventure would you like more information about? 1 - 6"
-    adv_input = gets.strip
+  def list_adventures(destination_name)
+    puts "Please select an adventure from 1-6"
+    adventure_arr = RoadtripAdventures::Adventure.list_adventure_names(destination_name)
 
+    puts adventure_arr.map.with_index{|d, index| "\t#{index+1}. #{d.name}, #{d.price}"}
+    puts "********************************************************"
+    puts "Please choose another destination 1-10 or 'exit' to leave."
   end
-
-
 
   def goodbye
     puts "So long!! May your journey be filled with new adventures and excitement!!"
