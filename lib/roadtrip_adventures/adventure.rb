7e7806c73e13a87242f68f16c014ca2b5cb4d433
jstone18 <jason.stone0808@gmail.com>
2019-02-03 11:53:37 -0500

    Cleaned up code

--- lib/roadtrip_adventures/adventure.rb
+++ lib/roadtrip_adventures/adventure.rb
@@ -1,35 +1,24 @@
 class RoadtripAdventures::Adventure
 
-  attr_accessor :name, :url
+  attr_accessor :name, :url, :price
 
-  # Users input selection of 1-10 will then provide 6 different adventures for that selected destination
+  def self.list_adventure_names(destination_name)
+    self.scrape_adventures(destination_name)
+  end
 
-
-
-  # Scrape method using url to create new Adventure objects
-  # push new objects into an array
-  def self.scrape_adventures
+  def self.scrape_adventures(destination_name)
     new_adventures_list = []
-    doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/usa/new-york-city")) # Just using one destination to create functionality
+
+    url = RoadtripAdventures::Destination.url_for_destination_name(destination_name)
+    doc = Nokogiri::HTML(open(url))
     adventures = doc.css("article.tours a")
 
     adventures[0..-2].map.with_index{ |adventure, index| # Returns 6 "adventure" options
     new_adventure = RoadtripAdventures::Adventure.new
     new_adventure.name = adventures[index].css("h3").text
+    new_adventure.price = adventures[index].css("span.tour__price").text
     new_adventure.url = adventures[index].attr("href")
-    new_adventures_list[index] = new_adventure
-    }
-    # binding.pry
+    new_adventures_list[index] = new_adventure }
   end
-
-  # Assign scrape method to a variable
-  # Itterate over the returned array from scrape method and return numbered list of "adventures"
-  def self.list_adventure_names
-    adventures_list = self.scrape_adventures
-    adventures_list.map.with_index{|a, i| puts "#{i+1}. #{a.name}"}
-  end
-
-
-
 
 end
