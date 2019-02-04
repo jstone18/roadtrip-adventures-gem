d6685948d265a8e9eecd97adaed5ab0a94cc31cb
jstone18 <jason.stone0808@gmail.com>
2019-02-04 03:02:55 -0500

    Added color to text output, removed comments, and eliminated some whitespace.

--- lib/roadtrip_adventures.rb
+++ lib/roadtrip_adventures.rb
@@ -1,13 +1,11 @@
 require "nokogiri"
 require "open-uri"
 require "pry"
+require "colorize"
+
 
 require_relative "./roadtrip_adventures/version"
 require_relative "./roadtrip_adventures/cli"
 require_relative "./roadtrip_adventures/destination"
 require_relative "./roadtrip_adventures/adventure"
-
-module RoadtripAdventures
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require_relative "./roadtrip_adventures/scraper"
--- lib/roadtrip_adventures/cli.rb
+++ lib/roadtrip_adventures/cli.rb
@@ -7,49 +7,50 @@ class RoadtripAdventures::CLI
   end
 
   def welcome
-    puts "Welcome to Roadtrip Adventures!"
+    puts "\nWELCOME TO ROADTRIP ADVENTURES!\n".colorize(:yellow)
   end
 
   def list_destinations
-     puts "Please select a destination from 1-10"
-
+     puts "**********DESTINATIONS**********\n".colorize(:blue)
      destination_arr = RoadtripAdventures::Destination.all
      puts  destination_arr.map.with_index{|destination, index| "\t#{index+1}. #{destination.name}"}
-     puts "********************************************************"
+     puts "\n********************************".colorize(:blue)
+     puts "Select a destination from 1-10:".colorize(:yellow)
+     puts "********************************".colorize(:blue)
      select_destination(destination_arr)
   end
 
   def select_destination(destination_arr)
     input = nil
+
     while input != 'exit'
       input = gets.to_i
 
       if input > 0 && input < 11
-        puts "********************************************************"
         destination_name = destination_arr[input-1].name.strip
         destination_url = destination_arr[input-1].url
-        puts "Here are the top 6 adventures for #{destination_name}\n"
+        # puts "\nHere are the top 6 adventures for #{destination_name}\n".colorize(:yellow)
+        puts "******************** ".colorize(:blue) + "#{destination_name.upcase} ADVENTURES".colorize(:yellow) + " ********************\n".colorize(:blue)
         list_adventures(destination_name)
+        puts "**********************************************************".colorize(:blue)
       else
-        puts "You've made an invalid choice! Make a selection from 1-10 or 'exit' to leave"
+        puts "You've made an invalid choice! Make a selection from 1-10 or 'exit' to leave".colorize(:light_blue)
         input = gets.strip
       end
     end
     goodbye if input == "exit"
-
   end
 
   def list_adventures(destination_name)
-    puts "Please select an adventure from 1-6"
     adventure_arr = RoadtripAdventures::Adventure.list_adventure_names(destination_name)
-
-    puts adventure_arr.map.with_index{|d, index| "\t#{index+1}. #{d.name}, #{d.price}"}
-    puts "********************************************************"
-    puts "Please choose another destination 1-10 or 'exit' to leave."
+    puts adventure_arr.map.with_index{|d, index| "#{index+1}. #{d.name}, #{d.price}"}
+    puts "\n**********************************************************".colorize(:blue)
+    puts "Please choose another destination 1-10 or 'exit' to leave.".colorize(:yellow)
   end
 
   def goodbye
-    puts "So long!! May your journey be filled with new adventures and excitement!!"
+    puts "So long!! May your journey be filled with new adventures and excitement!!".colorize(:yellow)
   end
+
 
 end
